7f5a2543fe9184c2145c423aefab7c71f7593893
Arman Bilge <armanbilge@gmail.com>
2022-07-02 23:49:01 +0000

    Add ScheduledTask

---
+++ core/jvm/src/main/scala/cats/effect/unsafe/ScheduledTask.scala
@@ -0,0 +1,31 @@
+/*
+ * Copyright 2020-2022 Typelevel
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats.effect.unsafe
+
+private[unsafe] final class ScheduledTask(
+    val at: Long,
+    val runnable: Runnable,
+    @volatile var canceled: Boolean = false
+) extends Runnable
+    with Comparable[ScheduledTask] {
+
+  def run(): Unit = canceled = true
+
+  def compareTo(that: ScheduledTask): Int =
+    java.lang.Long.compare(this.at, that.at)
+
+}
