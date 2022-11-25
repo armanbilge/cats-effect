80cec891555f30091af380d121824348109fabf7
Arman Bilge <armanbilge@gmail.com>
2022-11-25 04:24:31 +0000

    Pure Java `SyncRef`

--- kernel/jvm/src/main/java/cats/effect/kernel/SyncRef.java
+++ kernel/jvm/src/main/java/cats/effect/kernel/SyncRef.java
@@ -20,13 +20,67 @@ import java.util.concurrent.atomic.AtomicReferenceFieldUpdater;
 
 // defined in Java since Scala doesn't let us define static fields
 @SuppressWarnings({"rawtypes", "serial", "unchecked"})
-final class SyncRef extends AbstractSyncRef {
+class SyncRef extends Ref {
 
   private volatile Object value;
+  private final Sync F;
 
   SyncRef(Object F, Object value) {
-    super((Sync) F);
+    this.F = (Sync) F;
     this.value = value;
+  }
+
+  public Object get() {
+    return F.delay(() -> updater.get(this));
+  }
+
+  public Object set(Object a) {
+    return F.delay(
+        () -> {
+          updater.set(this, a);
+          return scala.runtime.BoxedUnit.UNIT;
+        });
+  }
+
+  public Object getAndSet(Object a) {
+    return F.delay(() -> updater.getAndSet(this, a));
+  }
+
+  public Object modify(scala.Function1 f) {
+    return F.delay(() -> {
+      Object b, c, u;
+      do {
+        c = updater.get(this);
+        scala.Tuple2 ub = (scala.Tuple2) f.apply(c);
+        u = ub._1();
+        b = ub._2();
+      } while (!updater.compareAndSet(this, c, u));
+      return b;
+    });
+  }
+
+  public Object tryModify(scala.Function1 f) {
+    return null;
+  }
+
+  public Object update(scala.Function1 f) {
+    return null;
+  }
+
+  public Object tryUpdate(scala.Function1 f) {
+    return null;
+  }
+
+  public Object access() {
+    return null;
+  }
+
+  public Object modifyState(cats.data.IndexedStateT state) {
+    return null;
+  }
+
+  public Object tryModifyState(cats.data.IndexedStateT state) {
+    return null;
   }
 
   static final AtomicReferenceFieldUpdater<SyncRef, Object> updater =
